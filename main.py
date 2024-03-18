49e244befd86b0ec88d6601bb12f1c723f9dbcf1
Peme969 ⚡ <136040410+peme969@users.noreply.github.com>
2024-03-17 19:13:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,29 +15,37 @@ def send_email(sender_email, sender_password, recipient_email, subject, message)
     server.login(sender_email, sender_password)
     server.sendmail(sender_email, recipient_email, msg.as_string())
     server.quit()
+def capitalizy(input_string):
+    return input_string.title()[0] + input_string.title()[1:]    
 def convert(time):
   from datetime import datetime, timezone, timedelta
   input_datetime = datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
   cst_offset = timedelta(hours=-6)
   cst_datetime = input_datetime.replace(tzinfo=timezone.utc) + cst_offset
   return cst_datetime.strftime("%Y-%m-%d %H:%M:%S")
-with requests.get('https://yarnharb-detect.vercel.app/check') as app:
+channel_id = os.eviron['channel_id']
+api_key = os.eviron['api_key'] # get an api key on the google cloud youtube data v3 api service
+with requests.get('https://channel-update-api.vercel.app/check') as app:
   aps = app.json()
   if aps['text'] == "No video uploaded":
     print("No video uploaded D:")
   elif aps['text'].startswith("New video"):
+    title = capitalizy(aps['channel_title'])
     cst_time = convert(aps['published'])
-    print(f"\033[92mYay! Yarnhub released {aps['title']} on {cst_time}\nWatch it here: {aps['url']}\033[0m")
+    print(f"\033[92mYay! {title} released {aps['title']} on {cst_time}\nWatch it here: {aps['url']}\033[0m")
     try:
-      sender_email = "mrcoderpeme@gmail.com"
-      sender_password = "wewg phhy oxsn urip"
-      recipient_email = "mrcoderpeme@gmail.com"
-      subject = "Yarnhub released a new video!"
-      message = f"Yarnhub released {aps['title']} on {cst_time}\nWatch it here: {aps['url']}"
+      sender_email = os.eviron['email']
+      sender_password = os.eviron['email_password']
+      recipient_email = os.eviron['email'] # sends to your email
+      subject = f"{title} released a new video!"
+      message = f"{title} released {aps['title']} on {cst_time}\nWatch it here: {aps['url']}"
       send_email(sender_email, sender_password, recipient_email, subject, message)
       print("Email Sent!")
     except Exception as e:
       print(f"Error! an error occured sending the email...\n{e}")
       exit(-1)
+  elif app.status_code == 400:
+      print("Please provide a channel id and api key or valid channel ids and api keys :D")
+      exit(-1)
   else:
-    print(f"Oh no! the api has encounreted an error! Status code: \n{app.status_code} Error: \n{app.text}")
+    print(f"Oh no! the api has encountered an error! Please create an issue in the issue tab. Status code: \n{app.status_code} Error: \n{app.text}")
